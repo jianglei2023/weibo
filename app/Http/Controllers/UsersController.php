a3a46382f9ebef33436a60a3f1f5119b75359e0e
vagrant <jianglei2023@foxmail.com>
2019-10-03 03:44:09 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class UsersController extends Controller
 {
-    //
+    //注册页面
     public function create()
     {
         return view('users.create');
     }
+
+    //显示用户
+    public function show(User $user)
+    {
+        return view('users.show',compact('user'));
+    }
+
+
+
+
+
 }
