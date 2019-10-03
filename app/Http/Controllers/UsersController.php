8b73212d35cad2dfe1e19c4b8c0a248a98b9b333
vagrant <jianglei2023@foxmail.com>
2019-10-03 08:21:45 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -46,5 +46,41 @@ class UsersController extends Controller
         return redirect()->route('users.show',[$user]);
     }
 
+    //编辑用户
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    //更新用户信息
+    /*
+        User 获取用户ID对应的用户实例
+        Request 用户更新的表单数据
+
+    */
+    public function update(User $user,Request $request)
+    {
+
+        //表单信息验证
+        $this->validate($request,[
+
+            'name'  =>  'required|max:50',
+            'password'  =>  'nullable|confirmed|min:6'
+
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+
+        //判断是否需要修改密码
+        if ($request->password) {
+            $data['password']=bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success','更新成功!');
+        return redirect()->route('users.show',$user->id);
+
+
+    }
 
 }
