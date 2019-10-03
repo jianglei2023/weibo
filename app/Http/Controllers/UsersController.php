f5bb910cc92124d3698dcba10d48837ab70f8be3
vagrant <jianglei2023@foxmail.com>
2019-10-03 09:49:24 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,6 +19,12 @@ class UsersController extends Controller
         ]);
     }
 
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index',compact('users'));
+    }
+
     //注册页面
     public function create()
     {
